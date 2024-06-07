e3092fddc0743a79b28842426a8d6b6c91863066
Onur "Epbaygun <64700935+dragon35-qw@users.noreply.github.com>
2024-06-07 18:51:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// Paw Care
+// 2272029 - Josephine Alvina Luwia
+// 2272041 - Rafael Cavin Emmanuel Tuasuun
+const notif = document.getElementById('notif');
+const close = document.getElementById('close');
+
+close.addEventListener('click', function () {
+   notif.style.display = 'none';
+});
+
+(() => {
+   const forms = document.querySelectorAll('.needs-validation');
+
+   Array.from(forms).forEach(form => {
+      form.addEventListener(
+         'submit',
+         event => {
+            event.preventDefault();
+
+            // Form is not valid
+            if (!form.checkValidity()) {
+               event.stopPropagation();
+            }
+
+            form.classList.add('was-validated');
+
+            // Form is valid
+            if (form.checkValidity()) {
+               sendEmail();
+               notif.style.display = 'block';
+               form.classList.remove('was-validated');
+               form.reset();
+               scrollTo(0, 0);
+            }
+         },
+         false
+      );
+   });
+})();
+
+// email
+function sendEmail() {
+   Email.send({
+      Host: 'smtp.elasticemail.com',
+      Username: 'pawcare.clinic@gmail.com',
+      Password: '3F993C3D46E8EA42D6D232A788CC383924DA',
+      To: document.getElementById('email').value,
+      From: 'pawcare.clinic@gmail.com',
+      Subject: 'Meeting Appointment',
+      Body:
+         'Hi there! <br><br> You have successfully booked an appointment for <u>' +
+         document.getElementById('patient').value +
+         '</u> on <u>' +
+         document.getElementById('date').value +
+         '</u>.' +
+         ' We have scheduled your appointment with <u>' +
+         document.getElementById('sel').value +
+         '</u>. We expect your presence <b>10 minutes</b> before the scheduled appointment. Thank you for making an appointment with us. Stay healthy! <br><br> Sincerely,<br>PawCare Clinic',
+   });
+}
